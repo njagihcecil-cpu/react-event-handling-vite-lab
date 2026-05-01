314b2e4deb97fb308e880b8b88fcd6a86072c6e4
njagih <njagih600@gmail.com>
2026-05-01 13:30:33 +0300

    first try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <div>
-      <PasswordInput/>
-      <SubmitButton/>
+      <PasswordInput />
+      <SubmitButton />
     </div>
   )
 }
--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,16 @@
 // Code PasswordInput Component Here
 
-function PasswordInput (){
-    return(
-        <></>
-    )
+function PasswordInput() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+
+  return (
+    <input
+      type="password"
+      onChange={handleChange}
+    />
+  );
 }
 
 export default PasswordInput;
